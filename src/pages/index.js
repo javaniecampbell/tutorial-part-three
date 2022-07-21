e9258c639c16077f09ec7fd2aba2258590572806
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2022-07-21 13:07:58 -0400

    Added pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import Layout from "../components/layout"
+export default () => (
+    <Layout style={{ margin: `3rem auto`, maxWidth: 600 }}>
+        <h1>Hi! I'm building a fake Gatsby site as a part of a tutorial!</h1>
+        <p>
+            What do I like to do? Lots of course, but definitely enjoy building websites.
+        </p>
+    </Layout>
+)
